0ff1eba94d9f34fe35d3c2217c74022bbc93e3a2
Evangelion-001 <65453924+Evangelion-001@users.noreply.github.com>
2020-06-02 11:48:02 +0200

    ligth treatment script
    first version only to pilot light

--- lightTreat.py
+++ lightTreat.py
@@ -45,7 +45,6 @@ client.on_message = on_message
 client.connect("localhost", 1883, 60)
 
 
-
 client.subscribe("light/value") #recupere l'information de l'état des lumières
 
 reflight=raffraichirref()
